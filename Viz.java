87c1ce81372895b4c69f420262c02bfcc5832e2e
Jack B. Du <JackBDu@nyu.edu>
2015-05-04 22:59:17 +0800

    readCSV fixed and tested

--- Viz.java
+++ Viz.java
@@ -7,7 +7,10 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.BufferedReader;
 public class Viz {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		InputStream is = new FileInputStream("data/mta-new-york-city-transit_20150404_2233/calendar.txt");
+		ArrayList<Map<String, String>> al = readCSV(is);
+		System.out.println(al.get(0).get("service_id"));
 	}
 
 	// reads a CSV stream
@@ -15,14 +18,15 @@ public class Viz {
 		ArrayList<Map<String, String>> al = new ArrayList<Map<String, String>>();
 		BufferedReader br = new BufferedReader(new InputStreamReader(csv));
 		String[] headerLine = br.readLine().split(","); // reads the header of the file
-		String[] line = br.readLine().split(","); // reads the first line of data
+		String line = br.readLine(); // reads the first line of data
 		while (line != null) {
+			String[] values = line.split(",");
 			Map<String, String> map = new HashMap<String, String>();
-			for (int i = 0; i < line.length; i++) {
-				map.put(headerLine[i], line[i]); // maps the header with corresponding value
+			for (int i = 0; i < values.length; i++) {
+				map.put(headerLine[i], values[i]); // maps the header with corresponding value
 			}
 			al.add(map); // adds the map to the ArrayList
-			line = br.readLine().split(",");
+			line = br.readLine();
 		}
 		br.close();
 		return al;
