eff5f88991d8fc5390391607c5bd55b70ff942ce
JackBDu <jd2804@nyu.edu>
2015-05-07 10:38:12 +0800

    different lines differen color

--- App.java
+++ App.java
@@ -20,29 +20,45 @@ import java.util.*;
 
 public class App extends JPanel {
 	ArrayList<Map<String, String>> shapes;
+	Color[] colors = new Color[7];
 	public App(ArrayList<Map<String, String>> shapes) {
 		this.setBackground(new Color(230, 230, 230));				// set background color to white
 		this.setFocusable(true);
 		this.shapes = shapes;
+		this.colors[0] = new Color(0,0,0);
+		this.colors[1] = new Color(0,0,255);
+		this.colors[2] = new Color(0,255,0);
+		this.colors[3] = new Color(0,255,255);
+		this.colors[4] = new Color(255,0,0);
+		this.colors[5] = new Color(255,0,255);
+		this.colors[6] = new Color(255,255,0); 
 	}
 
 	// paint the whole board
 	public void paint(Graphics g) {
+		int colorN = 0;
 		super.paint(g);
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g.setColor(Color.black);
+		g.setColor(this.colors[colorN]);
 		for(int i = 1; i < this.shapes.size(); i++) {
 			if (Integer.parseInt(this.shapes.get(i).get("shape_pt_sequence")) != 0) {
 				double startLat = Double.parseDouble(this.shapes.get(i-1).get("shape_pt_lat"));
 				double startLon = Double.parseDouble(this.shapes.get(i-1).get("shape_pt_lon"));
 				double endLat = Double.parseDouble(this.shapes.get(i).get("shape_pt_lat"));
 				double endLon = Double.parseDouble(this.shapes.get(i).get("shape_pt_lon"));
-				Double x1 = (startLat - 40.3) * 1000;
-				Double x2 = (endLat - 40.3) * 1000;
-				Double y1 = 600 - (startLon + 74.3) * 1000;
-				Double y2 = 600 - (endLon + 74.3) * 1000;
+				Double y1 = 600 - (startLat - 40.4) * 1000;
+				Double y2 = 600 - (endLat - 40.4) * 1000;
+				Double x1 = (startLon + 74.4) * 1000;
+				Double x2 = (endLon + 74.4) * 1000;
 				g.drawLine(x1.intValue(), y1.intValue(), x2.intValue(), y2.intValue());
+			} else {
+				if (colorN == this.colors.length - 1) {
+					colorN = 0;
+				} else {
+					colorN++;
+				}
+				g.setColor(this.colors[colorN]);
 			}
 		}
 	} // paint() ends
@@ -63,10 +79,6 @@ public class App extends JPanel {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
-
-		
-
-
 
 		while(true) {
 			app.update();
